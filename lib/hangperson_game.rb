3fefadaab46d44b58e1a0d6e2c4900207c3f1a8a
Joseph Cipollina <jcipoll1@binghamton.edu>
2017-02-02 02:29:04 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,14 +4,50 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  
+  attr_accessor :word, :guesses, :wrong_guesses
+  LOSE = 7
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
-
+  
+  
+  def guess(letter)
+    # only return true if valid guess
+    raise(ArgumentError) if letter.nil? ||letter.empty? || letter =~ /[^a-z]/i
+    letter.downcase!
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    if word.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+  end
+  
+  def word_with_guesses
+    @word.split('').map do |letter|
+      if @guesses.include?(letter)
+        letter
+      else 
+        "-"
+      end
+    end.join
+  end
+  
+  def check_win_or_lose
+    if word_with_guesses.match(/[a-z]{#{word.length}}/i)
+      :win
+    elsif @wrong_guesses.length==7
+      :lose
+    else
+      :play
+    end
+      
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
